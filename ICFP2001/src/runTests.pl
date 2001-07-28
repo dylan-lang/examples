c3c352bc15290a0642c57d1bed22b5708b5c19b4
Bruce Hoult <bruce@hoult.org>
2001-07-28 03:13:57 +0000

    add validation script and makefile item

---
+++ ICFP2001/src/runTests.pl
@@ -0,0 +1,20 @@
+#!/usr/local/bin/perl -w
+
+@testfiles =
+    qw(tests/test1
+       tests/test2
+       tests/test3
+       tests/test4
+       tests/test5
+       tests/test6
+       tests/test7
+       tests/example.txt
+       tests/big
+       );
+
+for my $f (@testfiles){
+    print "testing $f: ";
+    system("rm -f $f.out");
+    system("./icfp2001 180 <$f >$f.out 2>$f.debug");
+    system("./validate/validate $f $f.out");
+}

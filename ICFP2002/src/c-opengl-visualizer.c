419ae22c7f7ac0169b4ca4ef1f9dda60d1104751
Bruce Hoult <bruce@hoult.org>
2002-08-31 06:11:39 +0000

    Visualiser is getting better and better.\n

--- ICFP2002/src/c-opengl-visualizer.c
+++ ICFP2002/src/c-opengl-visualizer.c
@@ -15,9 +15,14 @@
 #include <GL/glut.h>
 #endif
 
+typedef unsigned int Boolean;
+#define TRUE  1
+#define FALSE 0
+
 #define allocateArray(T, n) (T*)malloc(n * sizeof(T))
 
 FILE *gSocket;
+int gUnixSocket;
 
 //------------------------------------------------------------------------
 
@@ -30,6 +35,25 @@ void readMyConfiguration(void)
     char buffer[4096];
     fgets(buffer, 4096, gSocket);
     sscanf(buffer, "%u %u %u", &gID, &gMaxWeight, &gMoney);
+}
+
+//------------------------------------------------------------------------
+
+#define MAX_ROBOTS 2000
+
+typedef struct
+{
+    Boolean playing;
+    unsigned int x;
+    unsigned int y;
+}
+Robot;
+
+Robot gPlayers[MAX_ROBOTS];
+
+void setOpenGLParametersForPlayer(unsigned int playerID)
+{
+    glColor3d(1.0, 0.0, 1.0);
 }
 
 //------------------------------------------------------------------------
@@ -129,11 +153,126 @@ void setOpenGLParametersForTileType(TileType type)
 
 //------------------------------------------------------------------------
 
+char *parseUnsigned(unsigned int *number, char *string)
+{
+    unsigned int length = 0;
+    while(string[length] >= '0' && string[length] <= '9')
+    {
+        ++length;
+    }
+
+    sscanf(string, "%u", number);
+
+    return string + length;
+}
+
+char *parsePlayerAction(unsigned int playerID, char *string)
+{
+    switch (*string)
+    {
+        case 'N':
+            ++gPlayers[playerID].y;
+            break;
+        case 'S':
+            --gPlayers[playerID].y;
+            break;
+        case 'E':
+            ++gPlayers[playerID].x;
+            break;
+        case 'W':
+            --gPlayers[playerID].x;
+            break;
+            
+        case 'P':
+        case 'D':
+        {
+            unsigned int packageID;
+            string += 2;
+            string = parseUnsigned(&packageID, string);
+            printf("Player %u picked up package id %u\n",
+                   playerID,
+                   packageID);
+        }
+        break;
+
+        case 'X':
+        {
+            unsigned int x, y;
+            string += 2;
+            string = parseUnsigned(&x, string);
+            string += 2;
+            string = parseUnsigned(&y, string);
+            printf("Player %u arrives at (%u, %u)\n",
+                   playerID, x, y);
+
+            gPlayers[playerID].x = x;
+            gPlayers[playerID].y = y;
+            gPlayers[playerID].playing = TRUE;
+        }
+        break;
+    }
+
+    return string;
+}
+
+char *parseServerResponse(char* string)
+{
+    ++string;
+
+    unsigned int playerID;
+    string = parseUnsigned(&playerID, string);
+    printf("Player id is %u\n", playerID);
+
+    ++string;
+
+    while (*string != '#' && *string != '\n')
+    {
+        string = parsePlayerAction(playerID, string);
+
+        if (*string == '\n')
+        {
+            break;
+        }
+        
+        ++string;
+    }
+
+    return string;
+}
+
+void readServerResponse(void)
+{
+    char buffer[32768];
+    fgets(buffer, 32768, gSocket);
+    printf("Received server state update: %s", buffer);
+
+    char* remainder = buffer;
+    while(*remainder != '\n')
+    {
+        remainder = parseServerResponse(remainder);
+    }
+}
+
+void readPackageList(void)
+{
+    char buffer[32768];
+    fgets(buffer, 32768, gSocket);
+    printf("Received package list: %s", buffer);
+}
+
+void sendAction(void)
+{
+    printf("sending \"100 Move N\\n\"\n");
+    send(gUnixSocket, "100 Move N\n", 11, 0);
+}
+
+//------------------------------------------------------------------------
+
 void setUpGameState(void)
 {
     readMap();
     readMyConfiguration();
-    //readServerResponse();
+    readServerResponse();
 }
 
 //------------------------------------------------------------------------
@@ -154,8 +293,22 @@ void display(void)
 
             glVertex2d(col,     row);
             glVertex2d(col + 1, row);
-            glVertex2d(col + 1, row+ 1);
+            glVertex2d(col + 1, row + 1);
             glVertex2d(col,     row + 1);
+        }
+    }
+
+    unsigned int playerID;
+    for (playerID = 0; playerID < MAX_ROBOTS; ++playerID)
+    {
+        if (gPlayers[playerID].playing)
+        {
+            setOpenGLParametersForPlayer(playerID);
+    
+            glVertex2d(gPlayers[playerID].x + 0.25, gPlayers[playerID].y + 0.25);
+            glVertex2d(gPlayers[playerID].x + 0.75, gPlayers[playerID].y + 0.25);
+            glVertex2d(gPlayers[playerID].x + 0.75, gPlayers[playerID].y + 0.75);
+            glVertex2d(gPlayers[playerID].x + 0.25, gPlayers[playerID].y + 0.75);
         }
     }
     
@@ -178,6 +331,10 @@ void reshape(int w, int h)
 
 void idle(void)
 {
+    readPackageList();
+    sendAction();
+    readServerResponse();
+    
     glutPostRedisplay();
 }
 
@@ -192,21 +349,25 @@ int main(int argc, char *argv[])
 
     struct sockaddr_in serverAddress;
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    gUnixSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct hostent *server
         = gethostbyname(serverHost);
-    
-    #if !defined (linux)
-    serverAddress.sin_len = 0;
-    #endif
+    if (server == NULL)
+    {
+        printf("Can't get host %s by name\n", serverHost);
+        exit(EXIT_FAILURE);
+    }
 
+#if !defined(linux)
+    serverAddress.sin_len = 0;
+#endif
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(serverPort);
     serverAddress.sin_addr.s_addr = *(in_addr_t*)(server->h_addr);
     memset(&(serverAddress.sin_zero), '\0', 8);
     
-    int connectionResult = connect(sock,
+    int connectionResult = connect(gUnixSocket,
                                    (struct sockaddr *)&serverAddress,
                                    sizeof(struct sockaddr));
     if (connectionResult == -1)
@@ -216,9 +377,9 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    send(sock, "Player\n", 7, 0);
+    send(gUnixSocket, "Player\n", 7, 0);
 
-    gSocket = fdopen(sock, "rb");
+    gSocket = fdopen(gUnixSocket, "rb");
     if (gSocket == NULL)
     {
         printf("Can't convert socket to FILE*\n");

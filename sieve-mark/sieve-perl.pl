f3c5e7bc58fd2b3f8bbe5097833b041830faadba
Bruce Hoult <bruce@hoult.org>
2006-12-11 19:44:57 +0000

    FIxed Erik's perl test

--- sieve-mark/sieve-perl.pl
+++ sieve-mark/sieve-perl.pl
@@ -2,18 +2,18 @@
 
 use strict;
 
-#my $limit = 5000000;
-my $limit = 500000;
-my @vec = 1..($limit-1);
+my $limit = 5000000;
+#my $limit = 500000;
+my @vec = 0..($limit-1);
 
 $vec[0] = 0;
+$vec[1] = 0;
 my $prime_count = 0;
 
-for (my $i = 0; $i < $limit; $i++) {
+for (my $i = 2; $i < $limit; $i++) {
     if ($vec[$i] != 0) {
 	$prime_count++;
-	my $prime = $i + 1;
-	for (my $j = 0; $j < $limit; $j += $prime) {
+	for (my $j = 2*$i; $j < $limit; $j += $i) {
 	    $vec[$j] = 0;
 	}
     }
